7f3c528037d47fcceb15f1d18b8f375628fa7051
Alexandre Portelinha <aleportelinha@gmail.com>
2015-09-05 10:36:49 -0300

    Ajustes da Activity de Main para Login

--- app/src/main/java/com/github/flavors/sample/Application/UI/Activities/LoginActivity.java
+++ app/src/main/java/com/github/flavors/sample/Application/UI/Activities/LoginActivity.java
@@ -1,16 +1,18 @@
-package com.github.flavors.sample;
+package com.github.flavors.sample.Application.UI.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.flavors.sample.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
     @Override
