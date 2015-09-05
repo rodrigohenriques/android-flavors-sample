c89bd991e9511910ee697ac5c81c07d6e4ea7bd1
Rodrigo Henriques <rodrigo.henriques@m4u.com.br>
2015-09-05 10:55:42 -0300

    renaming main activity to login activity

--- app/src/main/java/com/github/flavors/sample/application/ui/activities/LoginActivity.java
+++ app/src/main/java/com/github/flavors/sample/application/ui/activities/LoginActivity.java
@@ -1,16 +1,18 @@
-package com.github.flavors.sample;
+package com.github.flavors.sample.application.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.flavors.sample.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
     @Override
