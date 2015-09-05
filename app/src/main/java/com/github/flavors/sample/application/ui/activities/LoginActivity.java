1c6f044442f4487ac41331f5b6f0af42d23c490a
Rodrigo Henriques <rodrigo.henriques@m4u.com.br>
2015-09-05 11:04:08 -0300

    removing menu usages and files

--- app/src/main/java/com/github/flavors/sample/application/ui/activities/LoginActivity.java
+++ app/src/main/java/com/github/flavors/sample/application/ui/activities/LoginActivity.java
@@ -1,9 +1,7 @@
 package com.github.flavors.sample.application.ui.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 
 import com.github.flavors.sample.R;
 
@@ -13,27 +11,5 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
