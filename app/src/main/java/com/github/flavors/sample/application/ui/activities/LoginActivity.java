d6758f6e94af28680cfa7e0d1773d0e3c35285ac
Rodrigo Henriques <rodrigo.henriques@m4u.com.br>
2015-09-05 15:00:20 -0300

    adding custom java class sample

--- app/src/main/java/com/github/flavors/sample/application/ui/activities/LoginActivity.java
+++ app/src/main/java/com/github/flavors/sample/application/ui/activities/LoginActivity.java
@@ -2,8 +2,11 @@ package com.github.flavors.sample.application.ui.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.github.flavors.sample.R;
+import com.github.flavors.sample.domain.model.Greetings;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +14,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+
+        String greetings = new Greetings().raw();
+
+        Toast.makeText(this, greetings, Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/windows/java/com/github/flavors/sample/domain/model/Greetings.java
@@ -0,0 +1,9 @@
+package com.github.flavors.sample.domain.model;
+
+
+public class Greetings {
+
+    public String raw() {
+        return String.format("%s, %s, %s", "Ola", "Oi", "E ai parsa");
+    }
+}
